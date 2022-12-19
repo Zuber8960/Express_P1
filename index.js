02c9f9a00c49b691ed6c13b098c1e63aa1c9f60b
Zuber8960 <zuberahmad8960@gmail.com>
2022-12-19 18:38:07 +0530

    second_commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const cart_items = document.querySelector('#cart .cart-items');
+
+
+const parentContainer = document.getElementById('EcommerceContainer');
+parentContainer.addEventListener('click',(e)=>{
+
+    //for clicking cart to show on the screen and on click cancel will invisible.
+    if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
+        document.querySelector('#cart').style = "display:block;"
+    }
+    if (e.target.className=='cancel'){
+        document.querySelector('#cart').style = "display:none;"
+    }
+    
+    //adding the product in cart functionality.
+    if (e.target.className=='shop-item-button'){
+        const id = e.target.parentNode.parentNode.id;
+        // const name = document.querySelector(`#${id} h3`).innerText;
+        const name = e.target.parentNode.parentNode.firstElementChild.innerText;
+        const img_src = document.querySelector(`#${id} img`).src;
+        const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
+        let total_cart_price = document.querySelector('#total-value').innerText;
+        if (document.querySelector(`#in-cart-${id}`)){
+            alert('This item is already added to the cart');
+            return
+        }
+        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
+        const cart_item = document.createElement('div');
+        cart_item.classList.add('cart-row');
+        cart_item.setAttribute('id',`in-cart-${id}`);
+        total_cart_price = parseFloat(total_cart_price) + parseFloat(price);
+        // total_cart_price = Math.ceil(total_cart_price);
+        total_cart_price = total_cart_price.toFixed(2)
+        document.querySelector('#total-value').innerText = `${total_cart_price}`;
+        cart_item.innerHTML = `
+        <span class='cart-item cart-column'>
+        <img class='cart-img' src="${img_src}" alt="">
+            <span>${name}</span>
+    </span>
+    <span class='cart-price cart-column'>${price}</span>
+    <span class='cart-quantity cart-column'>
+        <input type="text" value="1">
+        <button>REMOVE</button>
+    </span>`
+        cart_items.appendChild(cart_item);
+
+        //for notification whenever added a product in cart will be shown a notification here.
+        const container = document.getElementById('container');
+        const notification = document.createElement('div');
+        notification.classList.add('notification');
+        notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
+        container.appendChild(notification);
+        setTimeout(()=>{
+            notification.remove();
+        },3000)
+    }
+    
+})
+
