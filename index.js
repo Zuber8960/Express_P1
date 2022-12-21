87d39cb9632d34545b8edfb01d8794e8ae76e433
Zuber8960 <zuberahmad8960@gmail.com>
2022-12-21 18:15:15 +0530

    fourth_commit

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const cart_items = document.querySelector('#cart .cart-items');
 
-window.addEventListener('DOMContentLoaded', () => {
-    axios.get("http://localhost:4000/products")
-        .then((responce) => {
-            if(responce.request.status === 200){
-                responce.data.forEach(element => {
-                    // console.log(element.id, element.title, element.price);
-                    showProductOnScreen(element);
-                });
-            }
-        })
-        .catch(err => console.log(err));
-})
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+        let responce = await axios.get("http://localhost:4000/products")
+        if (responce.request.status === 200) {
+            responce.data.forEach(element => {
+                // console.log(element.id, element.title, element.price);
+                showProductOnScreen(element);
+            });
+        }
+
+        let prom1 = await axios.get("http://localhost:4000/cart");
+
+        let numberOfProductsInCart = 0;
+        if(prom1.request.status===200){
+            await prom1.data.products.forEach(ele => {
+                numberOfProductsInCart++;
+                showInCart(ele);
+            })
+        }
+        document.querySelector('.cart-number').innerText = numberOfProductsInCart;
+
+    }catch(err){
+        console.log(err);
+    }
+});
+
 
 const marchent = document.querySelector('#merch-content');
 showProductOnScreen = (obj) => {
     const parentDiv = document.createElement('div');
 
-    parentDiv.setAttribute('id', `${obj.id}`);
+    parentDiv.setAttribute('id', `id-${(obj.id)}`);
     parentDiv.innerHTML = `
-    <div id="${obj.title}">
         <h3>${obj.title}</h3>
         <div class="image-container">
             <img class="prod-images" src="${obj.imageUrl}" alt="">
@@ -27,23 +40,40 @@ showProductOnScreen = (obj) => {
         <div class="prod-details">
             <span>$<span>${obj.price}</span></span>
             <button class="shop-item-button" type='button'>ADD TO CART</button>
-        </div>
-    </div>`
+        </div>`
 
-//     const secondDiv = document.createElement('div');
-//     secondDiv.setAttribute('id', obj.title);
-//     const h3tg = document.createElement('h3');
-//     h3tg.innerText = obj.title;
-//     secondDiv.appendChild(h3tg);
-//     secondDiv.innerHTML = `<div class="image-container">
-//     <img class="prod-images" src="${obj.imageUrl}" alt="">
-// </div>
-// <div class="prod-details">
-//     <span>$<span>${obj.price}</span></span>
-//     <button class="shop-item-button" type='button'>ADD TO CART</button>
-// </div>`
-// parentDiv.appendChild(secondDiv);
     marchent.appendChild(parentDiv);
+}
+
+let total_cart_price = document.querySelector('#total-value').innerText;
+showInCart = (obj) => {
+    const id = obj.id;
+    const title = obj.title;
+    const price = obj.price;
+    const imageUrl = obj.imageUrl;
+    const quantity = obj.cartItem.quantity;
+
+    const cart_item = document.createElement('div');
+    
+    cart_item.classList.add('cart-row');
+    cart_item.setAttribute('id', `in-cart-${id}`);
+    const updatedPrice = price*quantity;
+
+    total_cart_price = parseFloat(total_cart_price) + parseFloat(updatedPrice);
+    total_cart_price = total_cart_price.toFixed(2);
+
+    document.querySelector('#total-value').innerText = `${total_cart_price}`;
+    cart_item.innerHTML = `
+        <span class='cart-item cart-column'>
+            <img class='cart-img' src="${imageUrl}" alt="">
+            <span>${title}</span>
+        </span>
+        <span class='cart-price cart-column'>${price}</span>
+        <span class='cart-quantity cart-column'>
+            <input type="text" value="${quantity}" id="quantity-${id}">
+            <button>REMOVE</button>
+        </span>`
+    cart_items.appendChild(cart_item);
 }
 
 const parentContainer = document.getElementById('EcommerceContainer');
@@ -59,35 +89,37 @@ parentContainer.addEventListener('click', (e) => {
 
     //adding the product in cart functionality.
     if (e.target.className == 'shop-item-button') {
-        const id = e.target.parentNode.parentNode.id;
+        let id = e.target.parentNode.parentNode.id;
         // const name = document.querySelector(`#${id} h3`).innerText;
         const name = e.target.parentNode.parentNode.firstElementChild.innerText;
         const img_src = document.querySelector(`#${id} img`).src;
+        // const img_src = e.target.parentNode.classList.contains('prod-images').src;
         const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
-        let total_cart_price = document.querySelector('#total-value').innerText;
-        if (document.querySelector(`#in-cart-${id}`)) {
-            alert('This item is already added to the cart');
-            return
-        }
-        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) + 1
-        const cart_item = document.createElement('div');
-        cart_item.classList.add('cart-row');
-        cart_item.setAttribute('id', `in-cart-${id}`);
-        total_cart_price = parseFloat(total_cart_price) + parseFloat(price);
-        // total_cart_price = Math.ceil(total_cart_price);
-        total_cart_price = total_cart_price.toFixed(2);
-        document.querySelector('#total-value').innerText = `${total_cart_price}`;
-        cart_item.innerHTML = `
-        <span class='cart-item cart-column'>
-        <img class='cart-img' src="${img_src}" alt="">
-            <span>${name}</span>
-    </span>
-    <span class='cart-price cart-column'>${price}</span>
-    <span class='cart-quantity cart-column'>
-        <input type="text" value="1">
-        <button>REMOVE</button>
-    </span>`
-        cart_items.appendChild(cart_item);
+        // let total_cart_price = document.querySelector('#total-value').innerText;
+        //     if (document.querySelector(`#in-cart-${id}`)) {
+        //         alert('This item is already added to the cart');
+        //         return
+        //     }
+        //     document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) + 1
+
+        //     const cart_item = document.createElement('div');
+        //     cart_item.classList.add('cart-row');
+        //     cart_item.setAttribute('id', `in-cart-${id}`);
+        //     total_cart_price = parseFloat(total_cart_price) + parseFloat(price);
+        //     // total_cart_price = Math.ceil(total_cart_price);
+        //     total_cart_price = total_cart_price.toFixed(2);
+        //     document.querySelector('#total-value').innerText = `${total_cart_price}`;
+        //     cart_item.innerHTML = `
+        //     <span class='cart-item cart-column'>
+        //     <img class='cart-img' src="${img_src}" alt="">
+        //         <span>${name}</span>
+        // </span>
+        // <span class='cart-price cart-column'>${price}</span>
+        // <span class='cart-quantity cart-column'>
+        //     <input type="text" value="1" id="quantity-${id}">
+        //     <button>REMOVE</button>
+        // </span>`
+        //     cart_items.appendChild(cart_item);
 
         //for notification whenever added a product in cart will be shown a notification here.
         const container = document.getElementById('container');
@@ -98,6 +130,27 @@ parentContainer.addEventListener('click', (e) => {
         setTimeout(() => {
             notification.remove();
         }, 3000)
+
+
+        const productId = id.split("-")[1];
+        console.log(productId);
+        axios.post("http://localhost:4000/cart", { productId: productId })
+            .then((responce) => {
+                if (responce.request.status === 200) {
+                    console.log(responce.data);
+                    notifyOnScreen(responce.data.message);
+                    let cartProduct = document.getElementById(`quantity-${productId}`);
+                    // console.log('jjjj');
+                    cartProduct.value= +(cartProduct.value) +1;
+                    console.log(cartProduct.id, cartProduct.value);
+
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                notifyOnScreen(err.data.message);
+            });
+
     }
 
 
@@ -116,8 +169,9 @@ parentContainer.addEventListener('click', (e) => {
     //for remove button functionality.
     if (e.target.innerText == 'REMOVE') {
         let productId = e.target.parentNode.parentNode.id;
-        let total_cart_price = document.querySelector('#total-value').innerText;
-        total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${productId} .cart-price`).innerText).toFixed(2);
+        let qId = productId.split("-")[2];
+        // let total_cart_price = document.querySelector('#total-value').innerText;
+        total_cart_price = parseFloat(total_cart_price).toFixed(2) - (parseFloat(document.querySelector(`#${productId} .cart-price`).innerText)*parseFloat(document.getElementById(`quantity-${qId}`).value)).toFixed(2);
         document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) - 1
         document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
         e.target.parentNode.parentNode.remove()
@@ -125,3 +179,14 @@ parentContainer.addEventListener('click', (e) => {
 
 })
 
+
+
+function notifyOnScreen(massage) {
+    const notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.innerHTML = `<h4>${massage}<h4>`;
+    container.appendChild(notification);
+    setTimeout(() => {
+        notification.remove();
+    }, 3000)
+}
